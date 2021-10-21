8a945bdd431bb0937a9fa1bd25be3a56578e0202
Dominik Gedon <dgedon@suse.de>
2021-10-21 08:13:57 +0200

    Switch to SPDX license headers
    Signed-off-by: Dominik Gedon <dgedon@suse.de>

--- cmd/root.go
+++ cmd/root.go
@@ -1,26 +1,15 @@
-/*
-Copyright © 2021 Dominik Gedon <dgedon@suse.de>
+// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-FileCopyrightText: 2021 Dominik Gedon <dgedon@suse.de>
+// SPDX-FileCopyrightText: Copyright SUSE LLC
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
 package cmd
 
 import (
 	"fmt"
-	cobbler "github.com/cobbler/cobblerclient"
 	"net/http"
 	"os"
+
+	cobbler "github.com/cobbler/cobblerclient"
 
 	"github.com/spf13/cobra"
 
@@ -36,7 +25,7 @@ var rootCmd = &cobra.Command{
 	Use:   "cobbler",
 	Short: "Cobbler CLI client",
 	Long:  "An independent CLI to manage a Cobbler server.",
-	Run: func(cmd *cobra.Command, args []string) {},
+	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
