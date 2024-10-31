c1d05538c6200a16f113d8fd0a83c2e84ac3f732
Enno Gotthold <matrixfueller@gmail.com>
2024-10-31 10:56:37 +0100

    Fail if explicitly given config file is not existing

--- cmd/root.go
+++ cmd/root.go
@@ -110,10 +110,12 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		if verbose {
-			_, _ = fmt.Fprintln(os.Stdout, "Using config file:", viper.ConfigFileUsed())
-		}
+	err := viper.ReadInConfig()
+	if cfgFile != "" {
+		cobra.CheckErr(err)
+	}
+	if verbose {
+		_, _ = fmt.Fprintln(os.Stdout, "Using config file:", viper.ConfigFileUsed())
 	}
 }
 
